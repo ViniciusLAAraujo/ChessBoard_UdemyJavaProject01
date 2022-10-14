be6059d890013b60bf25d3547d0e00123ce38de9
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2022-10-14 20:24:18 -0300

    King possible moves implemented

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardlayer.Board;
+import boardlayer.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -13,16 +14,85 @@ public class King extends ChessPiece {
 	@Override
 	public String toString() {
 		/*
-		StringBuilder sb = new StringBuilder();
-		sb.append((char) 488);
-		return sb.toString();
-		*/
+		 * StringBuilder sb = new StringBuilder(); sb.append((char) 488); return
+		 * sb.toString();
+		 */
 		return "K";
+	}
+
+	private boolean canMove(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p==null || p.getColor() != getColor();
 	}
 
 	@Override
 	public int[][] possibleMoves() {
-		int[][] matx = new int [getBoard().getRows()][getBoard().getColumns()];
+		int[][] matx = new int[getBoard().getRows()][getBoard().getColumns()];
+		Position pos = new Position(0, 0);
+		// Above
+		pos.setValues(this.position.getRow() - 1, this.position.getColumn());
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			if (!isThereOpponentPiece(pos)) 
+				matx[pos.getRow()][pos.getColumn()] = 1;
+			else
+				matx[pos.getRow()][pos.getColumn()] = 2;
+		}
+		// Below
+		pos.setValues(this.position.getRow() + 1, this.position.getColumn());
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			if (!isThereOpponentPiece(pos)) 
+				matx[pos.getRow()][pos.getColumn()] = 1;
+			else
+				matx[pos.getRow()][pos.getColumn()] = 2;
+		}
+		// Right
+		pos.setValues(this.position.getRow(), this.position.getColumn() + 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			if (!isThereOpponentPiece(pos)) 
+				matx[pos.getRow()][pos.getColumn()] = 1;
+			else
+				matx[pos.getRow()][pos.getColumn()] = 2;
+		}
+		// Left
+		pos.setValues(this.position.getRow(), this.position.getColumn() - 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			if (!isThereOpponentPiece(pos)) 
+				matx[pos.getRow()][pos.getColumn()] = 1;
+			else
+				matx[pos.getRow()][pos.getColumn()] = 2;
+		}
+		//Upper Left (NW)
+		pos.setValues(this.position.getRow()-1, this.position.getColumn() - 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			if (!isThereOpponentPiece(pos)) 
+				matx[pos.getRow()][pos.getColumn()] = 1;
+			else
+				matx[pos.getRow()][pos.getColumn()] = 2;
+		}
+		//Upper Right (NE)
+		pos.setValues(this.position.getRow()-1, this.position.getColumn() + 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			if (!isThereOpponentPiece(pos)) 
+				matx[pos.getRow()][pos.getColumn()] = 1;
+			else
+				matx[pos.getRow()][pos.getColumn()] = 2;
+		}
+		//Lower Left (SW)
+		pos.setValues(this.position.getRow()+1, this.position.getColumn() - 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			if (!isThereOpponentPiece(pos)) 
+				matx[pos.getRow()][pos.getColumn()] = 1;
+			else
+				matx[pos.getRow()][pos.getColumn()] = 2;
+		}
+		//Lower Right (SE)
+		pos.setValues(this.position.getRow()+1, this.position.getColumn() + 1);
+		if (getBoard().positionExists(pos) && canMove(pos)) {
+			if (!isThereOpponentPiece(pos)) 
+				matx[pos.getRow()][pos.getColumn()] = 1;
+			else
+				matx[pos.getRow()][pos.getColumn()] = 2;
+		}
 		return matx;
 	}
 

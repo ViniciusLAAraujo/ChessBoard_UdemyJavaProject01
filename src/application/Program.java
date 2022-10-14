41279cc1e9d3ad4358a56ae77fcafd135051e6b1
Vinicius Araujo <vini.araujo.l.a@gmail.com>
2022-10-14 00:52:36 -0300

    Class Position v1.0

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardlayer.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-
+		Position pos = new Position(4, 8);
+		System.out.println(pos);
 	}
 
 }
